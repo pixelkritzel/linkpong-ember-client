8c840a7b49ed459dc7efeb0ee48f35a6f8f6a935
Timo Zoeller <timo@imazine.de>
2014-11-26 23:54:35 +0100

    Model

--- app/routes/store.js
+++ app/routes/store.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
+  model: function(model) {
+    console.log('This', this);
     return this.store.find('store', model.store_id);
   }
 });
