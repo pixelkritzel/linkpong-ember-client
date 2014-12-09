98962b2f2f9a614221aca3b8b66323ae127e2b90
Timo Zoeller <timo@imazine.de>
2014-12-09 21:39:52 +0100

    Bookmarklets: First Version

--- app/routes/store.js
+++ app/routes/store.js
@@ -2,8 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(model) {
-    console.log('This', this);
-    console.log('Model', model);
     return this.store.find('store', model.store_id);
+  },
+
+  setupController: function(controller, model) {
+    $.ajax('/bookmarklets/template', {async: false}).done(function(bookmarklet) {
+      controller.bookmarklet = bookmarklet;
+      this._super(controller, model);
+    }.bind(this));
   }
 });
