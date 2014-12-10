13340617d0a5622bea1ddece96f0d784ac12693a
Timo Zoeller <timo@imazine.de>
2014-12-10 12:31:40 +0100

    Load links by jQuery AJax from stores/:store_id/links

--- app/routes/links.js
+++ app/routes/links.js
@@ -2,9 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(model) {
-    console.log('This', this);
-    console.log(model);
-    console.log('Äh Model',this.modelFor('store').get('links'));
-    return this.currentModel;
+    return Ember.$.ajax('http://localhost:8080/stores/' + model.store_id + '/links', { async: true }).done(function(receivedData) {
+      return  this.store.createRecord('link', receivedData.links);
+    }.bind(this));
   }
 });
