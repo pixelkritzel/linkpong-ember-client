2b68de808a80b40d14eb788ff0ec3cb2d13c3c5c
Timo Zoeller <timo@imazine.de>
2014-12-14 23:06:46 +0100

    Revert "Load links by jQuery AJax from stores/:store_id/links"
    This reverts commit 13340617d0a5622bea1ddece96f0d784ac12693a.

--- app/routes/links.js
+++ app/routes/links.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(model) {
-    return Ember.$.ajax('http://localhost:8080/stores/' + model.store_id + '/links', { async: true }).done(function(receivedData) {
-      return  this.store.createRecord('link', receivedData.links);
-    }.bind(this));
+    console.log('This', this);
+    console.log(model);
+    console.log('Äh Model',this.modelFor('store').get('links'));
+    return this.currentModel;
   }
 });
